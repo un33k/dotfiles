cf0fca6c974fac08689e10c82ab87f8e7e4f7be1
Matías Aguirre <matiasaguirre@gmail.com>
2011-03-29 15:21:06 -0300

    Removed autojump.sh file

--- .bash/plugins/autojump.sh
+++ .bash/plugins/autojump.sh
@@ -1,4 +1,3 @@
-# Source autojump on BASH or ZSH depending on the shell
 #Copyright Joel Schaerer 2008, 2009
 #This file is part of autojump
 
@@ -14,8 +13,38 @@
 #
 #You should have received a copy of the GNU General Public License
 #along with autojump.  If not, see <http://www.gnu.org/licenses/>.
-if [ "$BASH_VERSION" ] && [ -n "$PS1" ] && echo $SHELLOPTS | grep -v posix >>/dev/null; then
-	. /etc/profile.d/autojump.bash
-elif [ "$ZSH_VERSION" ] && [ -n "$PS1" ]; then
-    . /etc/profile.d/autojump.zsh
+
+#This shell snippet sets the prompt command and the necessary aliases
+_autojump() 
+{
+        local cur
+        cur=${COMP_WORDS[*]:1}
+        while read i
+        do
+            COMPREPLY=("${COMPREPLY[@]}" "${i}")
+        done  < <(autojump --bash --completion $cur)
+}
+complete -F _autojump j
+#data_dir=${XDG_DATA_HOME:-$([ -e ~/.local/share ] && echo ~/.local/share || echo ~)}
+data_dir=$([ -e ~/.local/share ] && echo ~/.local/share || echo ~)
+export AUTOJUMP_HOME=${HOME}
+if [[ "$data_dir" = "${HOME}" ]]
+then
+    export AUTOJUMP_DATA_DIR=${data_dir}
+else
+    export AUTOJUMP_DATA_DIR=${data_dir}/autojump
 fi
+if [ ! -e "${AUTOJUMP_DATA_DIR}" ]
+then
+    mkdir "${AUTOJUMP_DATA_DIR}"
+    mv ~/.autojump_py "${AUTOJUMP_DATA_DIR}/autojump_py" 2>>/dev/null #migration
+    mv ~/.autojump_py.bak "${AUTOJUMP_DATA_DIR}/autojump_py.bak" 2>>/dev/null
+    mv ~/.autojump_errors "${AUTOJUMP_DATA_DIR}/autojump_errors" 2>>/dev/null
+fi
+
+AUTOJUMP='{ [[ "$AUTOJUMP_HOME" == "$HOME" ]] && (autojump -a "$(pwd -P)"&)>/dev/null 2>>${AUTOJUMP_DATA_DIR}/autojump_errors;} 2>/dev/null'
+if [[ ! $PROMPT_COMMAND =~ autojump ]]; then
+  export PROMPT_COMMAND="${PROMPT_COMMAND:-:} ; $AUTOJUMP"
+fi 
+alias jumpstat="autojump --stat"
+function j { new_path="$(autojump $@)";if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; cd "$new_path";fi }
